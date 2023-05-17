55acbbd0bfbc2880f825aa7fcd5245f71ad79964
Abby Deng <abby@datadoghq.com>
2023-05-17 11:56:57 -0700

    ch3 exercises 3.2

--- ch3/composite_types.go
+++ ch3/composite_types.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("----- 3.1 -----")
+
 	greetings := []string{"Hello", "Hola", "नमस्कार", "こんにちは", "Привіт"}
 	subSlice1 := greetings[0:2]
 	subSlice2 := greetings[1:4]
@@ -12,4 +14,13 @@ func main() {
 	fmt.Println(subSlice1)
 	fmt.Println(subSlice2)
 	fmt.Println(subSlice3)
+
+	fmt.Println("----- 3.2 -----")
+
+	message := "Hi 👩 and 👨"
+	fmt.Println(message[3])
+
+	runes := []rune(message)
+	fmt.Println(runes[3])
+	fmt.Println(string(runes[3]))
 }
