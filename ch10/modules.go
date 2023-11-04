e275e26a699d815452b7abfba8a7a0c8b57de3f1
Abby Deng <abby@datadoghq.com>
2023-11-04 14:43:25 -0700

    ch10 exercises 10.2

--- ch10/modules.go
+++ ch10/modules.go
@@ -1,7 +1,10 @@
+// Package main provides functions for mathematical addition.
 package main
 
-// ----- 10.1 -----
+// ----- 10.1 & 10.2 -----
 
+// Add implements mathematical addition of two integers.
+// See [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(i int, j int) int {
 	return i + j
 }
